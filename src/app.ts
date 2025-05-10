06450e90e096b225d1408a6419cc54c18bb6f12c
aady01 <aditya.aps000@gmail.com>
2025-05-10 22:33:58 +0530

    fix

--- src/app.ts
+++ src/app.ts
@@ -9,6 +9,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/api", doctorRoutes);
+app.get("/", (req, res) => {
+  res.json({
+    message: "uptime Bot",
+  });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
